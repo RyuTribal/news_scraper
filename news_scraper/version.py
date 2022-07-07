078b2ce30e1410d02f1a393fce03150fc8beb343
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2022-07-07 20:57:02 +0200

    added urls, version and finished api

---
+++ news_scraper/version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+To change the version of entire package, just edit this one location.
+"""
+
+__title__ = "news_scraper"
+__author__ = "Ivan Sedelkin, Suad Huseynli, Mohammed Shakir"
+__copyright__ = "Copyright 2022, EIOP"
+
+version_info = (0, 1, 0)
+__version__ = ".".join(map(str, version_info))
