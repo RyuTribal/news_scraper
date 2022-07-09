6f13d52597685285256bc7373f6880f0cd458b16
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2022-07-09 21:24:01 +0200

    created a spider that can crawl the whole domain

--- news_scraper/crawler.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-""" 
-This file contains all code related to the domain crawler 
-"""
-
-__title__ = "news_scraper"
-__author__ = "Ivan Sedelkin, Suad Huseynli, Mohammed Shakir"
-__copyright__ = "Copyright 2022, EIOP"
