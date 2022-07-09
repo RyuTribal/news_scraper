00aca599eb5ec2fe4dc0391c360694007d751a24
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2022-07-09 15:17:12 +0200

    added cralwer file

---
+++ news_scraper/crawler.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+""" 
+This file contains all code related to the domain crawler 
+"""
+
+__title__ = "news_scraper"
+__author__ = "Ivan Sedelkin, Suad Huseynli, Mohammed Shakir"
+__copyright__ = "Copyright 2022, EIOP"
